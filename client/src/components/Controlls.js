79c36a148f921c6247885eb1424eb1ae43161a19
Kishan Doshi <kishan.doshi88@gmail.com>
2021-10-05 00:30:33 -0400

    added go to question controlls

--- client/src/components/Controlls.js
+++ client/src/components/Controlls.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Controlls = ({currentQuestion, totalQuestions, changeQuestion}) => {
+
+    const [emptyArray, setEmptyArray] = useState([])
+
+    useEffect(() => {
+        setEmptyArray([...Array(totalQuestions)]);
+    }, [totalQuestions])
 
     const previousQuestion = () => {
         if(currentQuestion > 0) {
@@ -14,10 +20,20 @@ const Controlls = ({currentQuestion, totalQuestions, changeQuestion}) => {
         }
     }
 
+    const goToQuestion = index => {
+        changeQuestion(index);
+    }
+
     return (
         <div className="controlls-panel">
+            <span className="go-to">
+                {emptyArray.map((_, i) => (
+                    <button key={i} onClick={() => goToQuestion(i+1)} className={i+1==currentQuestion ? "go-to-question active" : "go-to-question"}>{i+1}</button>
+                ))}
+            </span>
             {currentQuestion != 1 && <button onClick={previousQuestion}><i className="fas fa-chevron-circle-left"></i></button>}
             {currentQuestion < totalQuestions && <button onClick={nextQuestion}><i className="fas fa-chevron-circle-right"></i></button>}
+
         </div>
     );
 }
